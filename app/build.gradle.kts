a65bdd709b82f90befe5f28567d1d5e8e14f5f57
Sean Larson <slarsoncreative@gmail.com>
2022-07-30 08:17:54 -0400

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+    defaultConfig {
+        applicationId = "com.bluelampcreative.udfdemo"
+        minSdk = 26
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+//            proguardFiles = getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility=  JavaVersion.VERSION_1_8
+        targetCompatibility =  JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.2.0"
+    }
+    packagingOptions.resources.excludes += setOf("/META-INF/{AL2.0,LGPL2.1}")
+}
+
+dependencies {
+    implementation(libs.androidx.ktxcore)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.compose)
+
+    implementation(project(":data"))
+    implementation(project(":domain"))
+}
---
+++ domain/src/main/java/com/bluelampcreative/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.bluelampcreative.domain
+
+class MyClass {
+}
